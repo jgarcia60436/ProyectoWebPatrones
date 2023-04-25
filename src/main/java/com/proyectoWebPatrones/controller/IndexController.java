affc0fb2347cd4b6ca457f9bf8e3f34f27c3358d
jgarcia60436 <jgarcia60436@ufide.ac.cr>
2023-04-25 09:56:21 -0600

    Decimoquinto Push - Conexion index y footer en plantilla

---
+++ src/main/java/com/proyectoWebPatrones/controller/IndexController.java
@@ -0,0 +1,21 @@
+package com.proyectoWebPatrones.controller;
+
+import com.proyectoWebPatrones.services.ProductoService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class IndexController {
+
+    @Autowired
+    private ProductoService ps;
+
+    @GetMapping("/")
+    public String inicio(Model model) {
+        var productos = ps.getProductos(); 
+        model.addAttribute("productos",productos);
+        return "/index"; 
+    }
+}
