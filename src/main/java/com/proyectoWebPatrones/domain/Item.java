c426c9d6989f2a141869e0619a8a9a87eaed9f12
SatiroMistico <107773521+SatiroMistico@users.noreply.github.com>
2023-04-25 21:15:46 -0600

    Tarea-adelanto HU-15

---
+++ src/main/java/com/proyectoWebPatrones/domain/Item.java
@@ -0,0 +1,24 @@
+package com.proyectoWebPatrones.domain;
+
+import lombok.Data;
+
+@Data
+public class Item extends Producto {
+    private int cantidad; //Almacenar la cantidad de items de un producto
+
+    public Item() {
+    }
+
+    public Item(Producto producto) {
+        super.setIdproducto(producto.getIdproducto());
+        super.setCategoria(producto.getCategoria());//---- hacer categoria-- credito
+        super.setNombreproducto(producto.getNombreproducto());
+        super.setDescripcion(producto.getDescripcion());
+        super.setPrecio(producto.getPrecio());
+        super.setCuidado(producto.getCuidado());
+        super.setCaracteristicas(producto.getCaracteristicas());
+        super.setImagen(producto.getImagen());
+        this.cantidad = 0;
+    }
+
+}
